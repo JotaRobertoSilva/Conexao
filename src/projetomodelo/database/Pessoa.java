d3c1371280f18ca2b94283733204e70b495eac96
JOSEROBERTO <JOSEROBERTO@192.168.1.104>
2015-08-16 18:48:08 -0300

    MUdança-

---
+++ src/projetomodelo/database/Pessoa.java
@@ -0,0 +1,41 @@
+package projetomodelo.database;
+
+public class Pessoa {
+	
+	private int pessoaId;
+	private String nome;
+	private String email;
+	private String address;	
+	
+	public int getPessoaId() {
+		return pessoaId;
+	}
+
+	public void setPessoaId(int pessoaId) {
+		this.pessoaId = pessoaId;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+}
